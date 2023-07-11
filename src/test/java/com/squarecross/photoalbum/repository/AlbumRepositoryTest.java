7c89e74a525e35fa4f3beca1912452c97a0d10a1
joona95 <waffle@vanpl.co.kr>
2023-07-11 22:06:44 +0900

    test: 정렬 및 키워드 포함 album 목록 조회 repository 테스트코드 추가

---
+++ src/test/java/com/squarecross/photoalbum/repository/AlbumRepositoryTest.java
@@ -0,0 +1,42 @@
+package com.squarecross.photoalbum.repository;
+
+import com.squarecross.photoalbum.domain.Album;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class AlbumRepositoryTest {
+
+    @Autowired
+    AlbumRepository albumRepository;
+
+    @Test
+    void testAlbumRepository() throws InterruptedException {
+        Album album1 = new Album();
+        Album album2 = new Album();
+        album1.setAlbumName("aaaa");
+        album2.setAlbumName("aaab");
+
+        albumRepository.save(album1);
+        TimeUnit.SECONDS.sleep(1);
+        albumRepository.save(album2);
+
+        List<Album> resDate = albumRepository.findByAlbumNameContainingOrderByCreatedAtDesc("aaa");
+        assertEquals("aaab", resDate.get(0).getAlbumName());
+        assertEquals("aaaa", resDate.get(1).getAlbumName());
+        assertEquals(2, resDate.size());
+
+        List<Album> resName = albumRepository.findByAlbumNameContainingOrderByAlbumNameAsc("aaa");
+        assertEquals("aaaa", resName.get(0).getAlbumName());
+        assertEquals("aaab", resName.get(1).getAlbumName());
+        assertEquals(2, resName.size());
+    }
+}
