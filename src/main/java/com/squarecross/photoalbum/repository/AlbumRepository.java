0af485fa95667c760a147a759e350d1190683563
joona95 <waffle@vanpl.co.kr>
2023-07-11 22:04:45 +0900

    feat: 정렬 및 키워드 포함 album 목록 조회 repository 구현

--- src/main/java/com/squarecross/photoalbum/repository/AlbumRepository.java
+++ src/main/java/com/squarecross/photoalbum/repository/AlbumRepository.java
@@ -4,9 +4,14 @@ import com.squarecross.photoalbum.domain.Album;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AlbumRepository extends JpaRepository<Album, Long> {
     Optional<Album> findByAlbumName(String albumName);
+
+    List<Album> findByAlbumNameContainingOrderByCreatedAtDesc(String keyword);
+
+    List<Album> findByAlbumNameContainingOrderByAlbumNameAsc(String keyword);
 }
