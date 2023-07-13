b15969207f13e045ebba185a742a6b579285d872
joona95 <waffle@vanpl.co.kr>
2023-07-13 18:15:36 +0900

    feat: 사진 상세 조회 기능 서비스 구현

---
+++ src/main/java/com/squarecross/photoalbum/service/PhotoService.java
@@ -0,0 +1,26 @@
+package com.squarecross.photoalbum.service;
+
+import com.squarecross.photoalbum.domain.Photo;
+import com.squarecross.photoalbum.dto.PhotoDto;
+import com.squarecross.photoalbum.mapper.PhotoMapper;
+import com.squarecross.photoalbum.repository.PhotoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.Optional;
+
+@Service
+public class PhotoService {
+    @Autowired
+    private PhotoRepository photoRepository;
+
+    public PhotoDto getPhoto(Long photoId) {
+        Optional<Photo> photo = photoRepository.findById(photoId);
+        if (photo.isPresent()) {
+            return PhotoMapper.convertToDto(photo.get());
+        } else {
+            throw new EntityNotFoundException(String.format("사진 아이디 %d로 조회되지 않았습니다", photoId));
+        }
+    }
+}
