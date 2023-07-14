26c747932861f9b62a68fc1bd60243c64c313376
joona95 <waffle@vanpl.co.kr>
2023-07-14 22:35:13 +0900

    feat: 사진 업로드 API 구현

--- src/main/java/com/squarecross/photoalbum/controller/PhotoController.java
+++ src/main/java/com/squarecross/photoalbum/controller/PhotoController.java
@@ -5,10 +5,11 @@ import com.squarecross.photoalbum.service.PhotoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/albums/{albumId}/photos")
@@ -20,5 +21,16 @@ public class PhotoController {
     public ResponseEntity<PhotoDto> getPhoto(@PathVariable("photoId") final long photoId) {
         PhotoDto photo = photoService.getPhoto(photoId);
         return new ResponseEntity<>(photo, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public ResponseEntity<List<PhotoDto>> uploadPhotos(@PathVariable("albumId") final long albumId,
+                                                       @RequestParam("photos") MultipartFile[] files) {
+        List<PhotoDto> photos = new ArrayList<>();
+        for (MultipartFile file : files) {
+            PhotoDto photoDto = photoService.savePhoto(file, albumId);
+            photos.add(photoDto);
+        }
+        return new ResponseEntity<>(photos, HttpStatus.OK);
     }
 }
