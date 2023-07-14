eedb58b199c93880b056f6f861e2d9d26f0a0fdd
joona95 <waffle@vanpl.co.kr>
2023-07-14 22:34:59 +0900

    feat: 사진 업로드 기능 서비스 구현

--- src/main/java/com/squarecross/photoalbum/Constants.java
+++ src/main/java/com/squarecross/photoalbum/Constants.java
@@ -2,4 +2,5 @@ package com.squarecross.photoalbum;
 
 public class Constants {
     public static final String PATH_PREFIX = "/Users/choejun-a/Desktop/GIT/photoalbum";
+    public static final int THUMB_SIZE = 300;
 }
--- src/main/java/com/squarecross/photoalbum/service/PhotoService.java
+++ src/main/java/com/squarecross/photoalbum/service/PhotoService.java
@@ -1,13 +1,25 @@
 package com.squarecross.photoalbum.service;
 
+import com.squarecross.photoalbum.Constants;
+import com.squarecross.photoalbum.domain.Album;
 import com.squarecross.photoalbum.domain.Photo;
 import com.squarecross.photoalbum.dto.PhotoDto;
 import com.squarecross.photoalbum.mapper.PhotoMapper;
+import com.squarecross.photoalbum.repository.AlbumRepository;
 import com.squarecross.photoalbum.repository.PhotoRepository;
+import org.imgscalr.Scalr;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
 import javax.persistence.EntityNotFoundException;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Optional;
 
 @Service
@@ -15,12 +27,71 @@ public class PhotoService {
     @Autowired
     private PhotoRepository photoRepository;
 
+    @Autowired
+    private AlbumRepository albumRepository;
+
+    private final String original_path = Constants.PATH_PREFIX + "/photos/original";
+    private final String thumb_path = Constants.PATH_PREFIX + "/photos/thumb";
+
     public PhotoDto getPhoto(Long photoId) {
         Optional<Photo> photo = photoRepository.findById(photoId);
         if (photo.isPresent()) {
             return PhotoMapper.convertToDto(photo.get());
         } else {
             throw new EntityNotFoundException(String.format("사진 아이디 %d로 조회되지 않았습니다", photoId));
+        }
+    }
+
+    public PhotoDto savePhoto(MultipartFile file, Long albumId) {
+        Optional<Album> album = albumRepository.findById(albumId);
+        if (album.isEmpty()) {
+            throw new EntityNotFoundException("앨범이 존재하지 않습니다");
+        }
+        String fileName = file.getOriginalFilename();
+        int fileSize = (int)file.getSize();
+        fileName = getNextFileName(fileName, albumId);
+        saveFile(file, albumId, fileName);
+
+        Photo photo = new Photo();
+        photo.setOriginalUrl("/photos/original/" + albumId + "/" + fileName);
+        photo.setThumbUrl("/photos/thumb/" + albumId + "/" + fileName);
+        photo.setFileName(fileName);
+        photo.setFileSize(fileSize);
+        photo.setAlbum(album.get());
+        Photo createdPhoto = photoRepository.save(photo);
+        return PhotoMapper.convertToDto(createdPhoto);
+    }
+
+    public String getNextFileName(String fileName, Long albumId) {
+        String fileNameNoExt = StringUtils.stripFilenameExtension(fileName);
+        String ext = StringUtils.getFilenameExtension(fileName);
+
+        Optional<Photo> photo = photoRepository.findByFileNameAndAlbum_AlbumId(fileName, albumId);
+
+        int count = 2;
+        while (photo.isPresent()) {
+            fileName = String.format("%s (%d).%s", fileNameNoExt, count, ext);
+            photo = photoRepository.findByFileNameAndAlbum_AlbumId(fileName, albumId);
+            count++;
+        }
+
+        return fileName;
+    }
+
+    private void saveFile(MultipartFile file, Long albumId, String fileName) {
+        try {
+            String filePath = albumId + "/" + fileName;
+            Files.copy(file.getInputStream(), Paths.get(original_path + "/" + filePath));
+
+            BufferedImage thumbImg = Scalr.resize(ImageIO.read(file.getInputStream()), Constants.THUMB_SIZE, Constants.THUMB_SIZE);
+            File thumbFile = new File(thumb_path + "/" + filePath);
+            String ext = StringUtils.getFilenameExtension(fileName);
+            if (ext == null) {
+                throw new IllegalArgumentException("No Extension");
+            }
+            ImageIO.write(thumbImg, ext, thumbFile);
+        } catch (Exception e) {
+            throw new RuntimeException("Could not store the file. Error: " + e.getMessage());
         }
     }
 }
